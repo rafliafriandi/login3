3f1701b89226716e17885c25ba674f67527f2599
unknown <rafliafriandi@gmail.com>
2024-10-23 16:25:20 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "weblanjut";
+
+$coonect = mysqli_connect($server,$username,$password,$database) or die("koneksi gagal");
+?>
